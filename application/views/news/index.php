736736cd3833ef9c47186e2ce5daa1884ab9d16e
dnevnick <dnevnick@gmail.com>
2015-10-05 13:40:06 +0500

    initial project version
    initial project version

---
+++ application/views/news/index.php
@@ -0,0 +1,11 @@
+<h2><?php echo $title; ?></h2>
+
+<?php foreach ($news as $news_item): ?>
+
+        <h4><?php echo $news_item['title']; ?></h4>
+        <div class="main">
+                <?php echo $news_item['text']; ?>
+        </div>
+        <p><a href="<?php echo site_url('news/'.$news_item['slug']); ?>">Подробнее</a></p>
+
+<?php endforeach; ?>
